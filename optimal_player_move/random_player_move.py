301f85eca32a8c2341dd02abee0102bf188c0932
Anan Faiyaz <anan.faiyaz@gmail.com>
2025-11-14 19:43:56 +0000

    creating random and optimal move files

---
+++ optimal_player_move/random_player_move.py
@@ -0,0 +1,13 @@
+import pygame
+import numpy as np
+import random
+
+
+
+class RandomPlayerMove:
+    def __init__(self, game, current_player, valid_moves):
+        self.game = game
+        self.current_player = current_player
+
+
+        def 
