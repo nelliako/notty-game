52e642ca16154acb25236a1a75dd4209d5d3ba95
Anan Faiyaz <anan.faiyaz@gmail.com>
2025-11-18 20:20:47 +0000

    Add method observe to playerC

--- optimal_player_move/playerC.py
+++ optimal_player_move/playerC.py
@@ -1,16 +1,41 @@
 import pygame
 import numpy as np
 import random
+import Class.Player from PlayerMove
 
-class Dummy():
-    def __init__(self,game, current_player, valid_actions):
+class playerC():
+    def __init__(self,hand, game,deck,current_player, valid_moves):
+        self.hand = hand
         self.game = game
+        self.deck = deck
         self.current_player = current_player
-        self.valid_actions=valid_actions
+        self.valid_moves=valid_moves
+
+    def observe(self, opp: PlayerX):
+        
+        playerXHand = opp.hand
+        playerYHand = TempObject.get_hand()
+        game_state = [ len(self.deck), len(self.hand), len(playerXHand), len(playerYHand), ]
+        
+
+
+
+
 
     def get_random_action(self):
-        self.temp=random.choice(self.valid_actions)
-        return self.temp
+        self.temp=random.choice(self.valid_moves)
+        parameter = random.choice([1,2,3])
+        if self.temp == PlayerMove.Draw:
+            return (self.temp,parameter)
+        return (self.temp,0)
     
     def updateAvailableMoves(self):
-        self.valid_actions.remove(self.temp)
+        self.valid_moves.remove(self.temp)
+        return self.valid_moves
+            #PlayerMove.DRAW,
+            #PlayerMove.TAKE,
+            #PlayerMove.DRAW_ONE,
+            #PlayerMove.DISCARD_CARD,
+            #PlayerMove.DISCARD_VALID_CARDS,
+            #PlayerMove.PASS,
+            #PlayerMove.END_TURN
--- optimal_player_move/playerDummy.py
+++ optimal_player_move/playerDummy.py
@@ -1,27 +1,26 @@
 import pygame
 import numpy as np
 import random
-import PlayerMove
+import Class.Player from PlayerMove
 
 class Dummy():
-    def __init__(self,game, current_player, valid_moves):
+    def __init__(self,game,deck,current_player, valid_moves):
         self.game = game
+        self.deck = deck
         self.current_player = current_player
         self.valid_moves=valid_moves
 
-    def get_random_action(self):
+    def get_random_move(self):
         self.temp=random.choice(self.valid_moves)
         parameter = random.choice([1,2,3])
-
         if self.temp == PlayerMove.Draw:
             return (self.temp,parameter)
-
-        return self.temp,
+        
+        return (self.temp,0)
     
     def updateAvailableMoves(self):
         self.valid_moves.remove(self.temp)
         return self.valid_moves
-    
             #PlayerMove.DRAW,
             #PlayerMove.TAKE,
             #PlayerMove.DRAW_ONE,
