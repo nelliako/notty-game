5a8d63ef5560ae6ae286d3c1dbc7c682876e0b21
pr0m3theuz <github.ferment664@simplelogin.com>
2025-12-02 16:30:01 +0000

    uncommented `see_screens()` in `main.py`

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from ui.see_cards import see_cards
 if __name__ == '__main__':
     # Choose one entry point to run by default
     # see_cards()
-    # see_screens()
+    see_screens()
     # run_game()
     # game_loop()
 
